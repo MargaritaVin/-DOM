1fe26d9bb91d821ccb3a700004d95fb8fee48cdd
MargaritaVin <ritasergienko@rambler.ru>
2022-09-06 17:12:28 +0300

    added styles.css, getUsers and createElement li in script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 "use strict";
 
+let albums = document.getElementById('albums')
+
+const getUsers = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+        if (!response.ok) {
+            throw new Error("Error: not found")
+        }
+        const date = await response.json();
+        date.map(({title}) => {
+            let li = document.createElement('li');
+            li.append(title);
+            li.classList.add('album_title');
+            albums.append(li)
+        });
+    } catch (err) {
+        console.error(err.message);
+    }
+};
+getUsers();
